0133de22ff663de63fd643c518ab9e791491c1fd
Oscar Zhang <oscarzhangzzq@gmail.com>
2025-10-05 16:33:15 -0700

    feat: Add contact me section

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,15 @@ import React from "react";
 
 export default function App() {
   return (
-    <div className='flex flex-col px-30'>
+    <div className='flex flex-col px-30 gap-y-12'>
       <header className='py-8 flex text-black items-center justify-between'>
         <p className='flex-1 text-2xl'>Oscar Zhang</p>
-        <a href='' className='px-10'>About</a>
+        <a href='#about' className='px-10'>About</a>
         <a href='#projects' className='px-10'>Experience</a>
-        <a className='pl-10'>Contact</a>
+        <a href='#contact' className='pl-10'>Contact</a>
       </header>
-      <hero className='flex items-center justify-between'>
-        <div className='flex flex-col justify-start gap-y-3 w-150'>
+      <hero id='about' className='flex items-center justify-between'>
+        <div className='flex flex-col justify-start gap-y-3 w-150 mt-5'>
           <p className='text-6xl font-bold'><span className='text-red-500'>Hi, </span>I'm Oscar</p>
           {/*<p className='text-xl'>I am a Computer Science student at UC Berkeley who enjoys building practical, full-stack applications. I have experience developing web apps using Python, Flask, and SQL, implementing everything from the database design to the user interface. Beyond web development, I have also constructed high-performance deep learning models and have a strong foundation in data structures and algorithms.</p>*/}
           <p className='text-xl'>I am a Computer Science student at UC Berkeley who enjoys building practical, full-stack applications.</p>
@@ -91,6 +91,26 @@ export default function App() {
           </div>
         </div>
       </div>
+      <div className='flex flex-col gap-y-5 pl-1'>
+        <p id='contact' className='text-5xl font-bold'>Contact me</p>
+        <div className='flex gap-x-3 justify-content items-center'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+          </svg>
+          <p className='text-lg'>oscarzhangdev AT gmail DOT com</p>
+        </div>
+        <a href='/Oscar-Zhang-Resume.pdf' className='flex gap-x-3 justify-content items-center'>
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-user-icon lucide-file-user size-10"><path d="M14 2v4a2 2 0 0 0 2 2h4"/><path d="M15 18a3 3 0 1 0-6 0"/><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7z"/><circle cx="12" cy="13" r="2"/></svg>
+          <p className='text-lg'>Download resume</p>
+        </a>
+        <div className='flex gap-x-5 mt-3'>
+          <a href='https://github.com/oscarzzq' target='_blank'><img src='public/github-mark.svg' alt='Github logo' class='w-15' ></img></a>
+          <a href='https://www.linkedin.com/in/oscar-ziqian-zhang/' target='_blank'><img src='public/InBug-Black.png' alt='Linkedin logo' className='h-15'></img></a>
+        </div>
+      </div>
+      <footer className='flex justify-center items-center'>
+        <p className='my-10'>Copyright © Oscar Zhang 2025</p>
+      </footer>
     </div>
   );
 }
